f942a46e366b18def967781cab933892ef73dfaf
Poltorak <kajor3k@gmail.com>
2017-01-14 14:43:32 +0100

    added 4 test cases according to table cases - TC3-when element in seqence is first, TC4-when element in seqence is last, TC5-when element in seqence is middle, TC6-when element is not in sequence
    Signed-off-by: Poltorak <kajor3k@gmail.com>

---
+++ src/test/java/edu/iis/mto/bsearch/FirstElem.java
@@ -0,0 +1,49 @@
+package edu.iis.mto.bsearch;
+
+import static org.junit.Assert.*;
+
+
+
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore; 
+import org.junit.runner.Result; 
+import org.junit.runner.notification.Failure;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult; 
+
+
+public class FirstElem{
+BinarySearch bs = new BinarySearch();
+SearchResult sr = new SearchResult();
+int key = 2;
+int seq[] = {2,5,3,4};
+
+
+	@Before
+	public void init(){
+		
+		sr = BinarySearch.search(key, seq);
+		
+		
+	
+		
+		
+		
+	}
+	@Test
+	public void isFound(){
+		
+		assertSame(true,sr.isFound());
+	}
+	@Test
+	public void isFirst(){
+		
+		assertSame("( elem at position 1 )",sr.toString());
+	}
+		
+	
+}
